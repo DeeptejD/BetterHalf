839d666a236ccdd66f5de6afcae201b3d7c51055
salelkarayush <salelkarayush@gmail.com>
2023-08-23 12:59:44 +0530

    Login works now fr

--- register.php
+++ register.php
@@ -18,10 +18,10 @@ if(isset($_POST['submit'])){
       $error[] = 'user already exist!';
 
    }else{
-
-      if($pswd != $cpswd){
-         $error[] = 'password not matched!';
-      }else{
+        if(($name == "")||($email == "")||($pass != $cpass)){
+         $error[] = 'Registration Failed. Please recheck your entered details.';
+        }
+        else{
          $insert = "INSERT INTO `register`(user_name, user_email, user_password) VALUES('$name','$email','$pass')";
          mysqli_query($conn, $insert);
          header('location:login.php');
@@ -58,24 +58,23 @@ if(isset($_POST['submit'])){
                 <div class="form">
                     <h1 class="deez">Create an account</h1>
                     <form action="" method="POST">
-
-                        <?php
-                            if(isset($error)){
-                                foreach($error as $error){
-                                    echo '<span class="error-msg">'.$error.'</span>';
+                            <?php
+                                if(isset($error)){
+                                    foreach($error as $error){
+                                        echo '<span color="white" class="error-msg">'.$error.'</span>';
+                                    };
                                 };
-                            };
-                        
-                        ?>
+                            
+                            ?>
                         <div class="nameinput">
                             Name
                             <br>
-                            <input type="text" placeholder="Enter Your Name" name="username">
+                            <input id="name" type="text" placeholder="Enter Your Name" name="username">
                         </div>
                         <div class="nameinput">
                             Email Address
                         <br>
-                        <input type="email" placeholder="Enter You Email Id" name="email">
+                        <input id="email" type="email" placeholder="Enter You Email Id" name="email">
                         </div>
                         <div class="nameinput">
                             Password
@@ -90,6 +89,36 @@ if(isset($_POST['submit'])){
                         <input id="sbmitbtn" type="submit" value="Submit" name="submit">
                     </form>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-            <script src="index.js" async defer></script>
+            <script>
+                    $("#sbmitbtn").on("click",function(){
+        // var deez = $("#pass").value;
+        var deez = document.getElementById("pass").value;
+        var deeze = document.getElementById("email").value;
+        var deezn = document.getElementById("name").value;
+        var cnfpswd = document.getElementById("cnfrm-pass").value;
+        
+
+        if((deezn.length == 0)) {
+            alert("Enter A Name");
+            document.getElementById("pass").value = "";
+            document.getElementById("cnfrm-pass").value = "";
+        }
+        if((deeze.length == 0)) {
+            alert("Enter Valid Email Please");
+            document.getElementById("pass").value = "";
+            document.getElementById("cnfrm-pass").value = "";
+        }
+        if ((deez !== cnfpswd)) {
+            alert("The Passwords entered differ!");
+            document.getElementById("pass").value = "";
+            document.getElementById("cnfrm-pass").value = "";
+        }
+        if((deez.length < 8)) {
+            alert("Password must exceed 8 characters");
+            document.getElementById("pass").value = "";
+            document.getElementById("cnfrm-pass").value = "";
+        }
+    })
+            </script>
     </body>
 </html>
