6017f969b4b40f2fc599f0bffbbbc395efaefc43
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-09-19 22:27:40 +0530

    fixed a tiny bug that caused a weird line to appear behind the nav when hovered

--- src/modules/authentication/register.php
+++ src/modules/authentication/register.php
@@ -82,7 +82,6 @@ if (isset($_POST['submit'])) {
     <?php include '../partials/head-content.php'; ?>
     <title>Register</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- <link rel="stylesheet" href="../../build/css/tailwind.css"> -->
 </head>
 
 <body>
@@ -171,13 +170,14 @@ if (isset($_POST['submit'])) {
         </script> -->
     <div class="bg-cover bg-center overflow-hidden h-screen w-screen flex flex-col px-5"
         style="background-image: url('../../images/dashboard/background-2.jpg');">
-        <div class="m-4 mb-0" style="backdrop-filter: blur(8px);">
+        <div class="m-4 mb-0">
             <nav class="w-full h-12 rounded-full p-2 bg-gray-100 flex flex-row justify-between bg-opacity-25 shadow-2xl"
                 style="backdrop-filter: blur(8px);">
                 <a href=" #" class=" m-2 text-semibold flex justify-center items-center font-semibold pl-6">Logo
                     goes here</a>
-                <a href="login.php" class=" m-2 text-semibold flex justify-center items-center font-semibold pr-6">Log
-                    In</a>
+                <div class=""><a href="login.php"
+                        class="m-1 text-semibold flex justify-center items-center font-semibold mr-6 rounded-xl hover:shadow-2xl">Log
+                        In</a></div>
             </nav>
         </div>
         <div class="flex-grow bg-white m-4 rounded-xl flex flex-row bg-opacity-25 shadow-2xl"
@@ -189,7 +189,7 @@ if (isset($_POST['submit'])) {
             <div class="flex-grow rounded-r-xl ml-5 p-5 flex flex-col justify-center items-center">
                 <form action="register.php" method="POST" class="w-full py-3 px-2">
                     <h1 class="text-gray-100  xl:text-4xl lg:text-3xl sm:text-lg font-bold mb-3">
-                        Create an Account</h1>
+                        Create an account</h1>
                     <?php
                     if (isset($error)) {
                         foreach ($error as $error) {
@@ -203,23 +203,23 @@ if (isset($_POST['submit'])) {
                     <div class="pt-2">
                         <label for="name" class="text-white text-lg font-semibold shadow-2xl pl-2">Name</label>
                         <input type="text" placeholder="Enter your full name" name="username" id="name"
-                            class="w-full rounded-2xl shadow-2xl bg-gray-200 p-5  focus:outline-none">
+                            class="w-full rounded-2xl shadow-2xl bg-white p-5  focus:outline-none">
                     </div>
                     <div class="pt-2">
                         <label for="email" class="text-white text-lg font-semibold shadow-2xl pl-2">Email</label>
                         <input type="emai" placeholder="johndoe@example.com" name="email" id="email"
-                            class="w-full rounded-2xl shadow-2xl bg-gray-200 p-5  focus:outline-none">
+                            class="w-full rounded-2xl shadow-2xl bg-white p-5  focus:outline-none">
                     </div>
                     <div class="pt-2">
                         <label for="password" class="text-white text-lg font-semibold shadow-2xl pl-2">Password</label>
                         <input type="password" name="password" id="pass" placeholder="Min 8 characters"
-                            class="w-full rounded-2xl shadow-2xl bg-gray-200 p-5  focus:outline-none">
+                            class="w-full rounded-2xl shadow-2xl bg-white p-5  focus:outline-none">
                     </div>
                     <div class="pt-2">
                         <label for="cpassword" class="text-white text-lg font-semibold shadow-2xl pl-2">Confirm
                             Password</label>
                         <input type="password" placeholder="* * * * * * * *" name="cpassword" id="cnfrm-pass"
-                            class="w-full rounded-2xl shadow-2xl bg-gray-200 p-5  focus:outline-none">
+                            class="w-full rounded-2xl shadow-2xl bg-white p-5  focus:outline-none">
                     </div>
                     <div class="flex flex-row justify-end pt-11">
                         <input type="submit" name="submit" id="submit"
@@ -229,6 +229,47 @@ if (isset($_POST['submit'])) {
             </div>
         </div>
     </div>
+
+    <script>
+        document.getElementById("submit").addEventListener("click", function () {
+            document.getElementById("name").style.border = "none";
+            document.getElementById("email").style.border = "none";
+            document.getElementById("pass").style.border = "none";
+            document.getElementById("cnfrm-pass").style.border = "none";
+
+            var deezn = document.getElementById("name").value;
+            var deeze = document.getElementById("email").value;
+            var deez = document.getElementById("pass").value;
+            var cnfpswd = document.getElementById("cnfrm-pass").value;
+
+            var isValid = true;
+
+            if (deezn.length === 0) {
+                isValid = false;
+                document.getElementById("name").style.border = "2px solid red";
+            }
+
+            if (deeze.length === 0 || !deeze.includes("@") || !deeze.includes(".")) {
+                isValid = false;
+                document.getElementById("email").style.border = "2px solid red";
+            }
+
+            if (deez.length < 8) {
+                isValid = false;
+                document.getElementById("pass").style.border = "2px solid red";
+            }
+
+            if (deez !== cnfpswd) {
+                isValid = false;
+                document.getElementById("cnfrm-pass").style.border = "2px solid red";
+            }
+
+            if (!isValid) {
+                // Prevent form submission if any validation fails
+                event.preventDefault();
+            }
+        });
+    </script>
 
 </body>
 
