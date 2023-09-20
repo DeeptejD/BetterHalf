e82c6b593d533c410f36a611c1f2306ee40d0725
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-09-20 12:42:01 +0530

    Forgot Password page is responsive

--- src/modules/authentication/forgot-password/forgot-password.php
+++ src/modules/authentication/forgot-password/forgot-password.php
@@ -16,7 +16,7 @@
         <input type="email" name="email" required>
         <button type="submit">Send Reset Link</button>
     </form> -->
-    <div class="bg-cover bg-center overflow-hidden h-screen w-screen flex flex-col px-5"
+    <div class="bg-cover bg-center md:overflow-hidden h-screen w-screen flex flex-col px-5"
         style="background-image: url('../../../images/dashboard/background-2.jpg');">
         <div class="m-4 mb-0">
             <nav class="w-full h-12 rounded-full p-2 bg-gray-100 flex flex-row justify-between bg-opacity-25 shadow-2xl"
@@ -32,15 +32,17 @@
                 </div>
             </nav>
         </div>
-        <div class="flex-grow bg-white m-4 rounded-xl flex flex-row bg-opacity-25 shadow-2xl"
+        <div class="flex-grow bg-white m-4 rounded-xl flex md:flex-row flex-col bg-opacity-25 shadow-2xl"
             style="backdrop-filter: blur(8px);">
-            <div class="bg-gray-900 rounded-l-xl w-1/3 h-full overflow-hidden">
+            <div class="bg-gray-900 md:rounded-l-xl rounded-t-xl md:w-1/3 w-full md:h-full h-1/3 overflow-hidden">
                 <img src="../../../images/forgot-password/forgot-side.gif" alt=""
-                    class="object-cover rounded-l-xl shadow-xl h-full w-full object-center transition transform duration-500 hover:scale-110  ">
+                    class="hidden md:block object-cover rounded-l-xl shadow-xl h-full w-full object-center transition transform duration-500 hover:scale-110  ">
+                <img src="../../../images/forgot-password/small-screen.gif" alt=""
+                    class="md:hidden block object-cover rounded-t-xl shadow-xl h-full w-full object-center transition transform duration-500 hover:scale-110  ">
             </div>
-            <div class="flex-grow rounded-r-xl ml-5 p-5 flex flex-col justify-center items-center">
+            <div class="flex-grow rounded-r-xl md:ml-5 p-5 flex flex-col justify-center items-center">
                 <form action="send-reset-link.php" method="post" class="w-full py-6 px-2">
-                    <h1 class="text-gray-100  xl:text-4xl lg:text-3xl sm:text-lg font-bold pb-3 mb-6">
+                    <h1 class="text-gray-100  text-2xl md:text-3xl font-bold md:pb-3 md:mb-6 pb-1 mb-2">
                         Enter your Email</h1>
                     <div class="pt-2">
                         <label for="email" class="text-white text-lg font-semibold shadow-2xl pl-2">Email</label>
@@ -49,7 +51,7 @@
                     </div>
                     <div class="flex flex-row justify-end pt-11">
                         <input type="submit" name="submit" id="submit"
-                            class="w-1/5 text-center p-5 h-full rounded-xl bg-slate-200 hover:bg-lime-100 active:bg-lime-200  active:shadow-inner font-semibold transition transform duration-500 hover:scale-110 focus:outline-none" />
+                            class="md:w-1/5 text-center p-5 h-full rounded-xl bg-slate-200 hover:bg-lime-100 active:bg-lime-200  active:shadow-inner font-semibold transition transform duration-500 hover:scale-110 focus:outline-none" />
                     </div>
                 </form>
             </div>
