4e1d107bbb9b25153238501495473556905ec3dc
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-11-01 00:49:45 +0530

    search was fixed

--- src/modules/chat/php/search.php
+++ src/modules/chat/php/search.php
@@ -1,12 +1,13 @@
 <?php
     session_start();
-    include_once "config.php";
+    include_once "../../config.php";
 
-    $outgoing_id = $_SESSION['unique_id'];
+    $outgoing_id = $_SESSION['user_email'];
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
 
-    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') ";
-    $output = "";
+$sql = "SELECT * FROM register WHERE NOT user_email = '{$outgoing_id}' AND (user_name LIKE '{$searchTerm}%')";
+
+$output = "";
     $query = mysqli_query($conn, $sql);
     if(mysqli_num_rows($query) > 0){
         include_once "data.php";
