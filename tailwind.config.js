d0ca52003f6efa4662a1b8375a272721f6d24e7a
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-11-11 20:00:12 +0530

    Landing Page and About page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ["./**/*.php", "./**/*.html"],
+  // darkMode: 'class',
+  content: ["./**/*.php", "./**/*.html"
+  ],
   theme: {
     extend: {
-        colors: {
-        brand: "#ec679e"
-        }
+
     },
   },
   plugins: [],
